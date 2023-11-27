26c5b0b9899220c999d95324232bdb2322bb9b1a
OlivierBldn <blandin.developpement@gmail.com>
2023-11-27 16:19:55 -0500

    Delete API, DB, and Global Config files

--- config/cfg_apiConfig.php
+++
@@ -1,3 +0,0 @@
-<?php // path: config/cfg_apiConfig.php
-
-const __API_KEY__ = 'P654fgd$!FDOPD543+sd;vsdKFSQJf/z6fsqfd3svxc4?PJFJp54f66647POFJEp^fezf64f';
--- config/cfg_gobalConfig.php
+++
@@ -1,6 +0,0 @@
-<?php // path: config/cfg_globalConfig.php
-
-const __WEBSITE_URL__ = 'apidesignpatern';
-const __UNPROTECTED_ROUTES__ = [
-    '/auth/login', '/register'
-];
